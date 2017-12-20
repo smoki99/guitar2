282cca999a8f84d01b41505360cd17d8bcaba826
christian mueller <smoki99@googlemail.com>
2017-12-20 20:45:02 +0100

    base client server ajax

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email"/>
+    <br>
+    <input type="password" name="password" v-model="password" placeholder="password"/>
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationServer from '@/services/AuthenticationService'
+export default {
+  name: 'HelloWorld',
+  methods: {
+    async register () {
+      const response = await AuthenticationServer.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
