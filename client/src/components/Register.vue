ef572d1481de5b9becb2d1c5401779df154d9805
christian mueller <smoki99@googlemail.com>
2017-12-21 20:20:24 +0100

    added more structure and sql to backend and vuetify to frontend

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email"/>
-    <br>
-    <input type="password" name="password" v-model="password" placeholder="password"/>
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" v-model="email" placeholder="email"/>
+          <br>
+          <input type="password" name="password" v-model="password" placeholder="password"/>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -15,17 +26,21 @@ export default {
   name: 'HelloWorld',
   methods: {
     async register () {
-      const response = await AuthenticationServer.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationServer.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   },
   data () {
     return {
       email: 'abc',
-      password: '123'
+      password: '123',
+      error: null
     }
   }
 }
@@ -33,5 +48,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
