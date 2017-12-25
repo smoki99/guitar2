9e69b1d0f98b88a3fa755fbac7aff746ccdb37ea
christian mueller <smoki99@googlemail.com>
2017-12-25 13:35:54 +0100

    include store vuex and login process

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,27 +3,22 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <form 
-            name="tab-tracker-from"
-            autocomplete="off">
-            <v-text-field
-              label="Email"
-              v-model="email"/>
-            <br>
-            <v-text-field
-              label="Password"
-              type="password"
-              autocomplete="new-password"
-              v-model="password"/>          
-          </form>
+          <v-text-field
+            label="Email"
+            v-model="email"/>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"/>
           <br>
           <div class="error" v-html="error"></div>
           <br>
-          <v-btn class="cyan" dark @click="register">Register</v-btn>
+          <v-btn class="cyan" dark @click="login">Login</v-btn>
         </div>
       </div>
     </v-flex>
@@ -35,12 +30,13 @@ import AuthenticationServer from '@/services/AuthenticationService'
 export default {
   name: 'HelloWorld',
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationServer.register({
+        const response = await AuthenticationServer.login({
           email: this.email,
           password: this.password
         })
+        debugger
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
